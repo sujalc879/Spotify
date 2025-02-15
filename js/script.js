31d9a144317f21d8e30f5ef71e27d780d05f2656
sujalc879 <142300159+sujalc879@users.noreply.github.com>
2025-02-15 20:53:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${currFolder}/`)
+    let a = await fetch(`https://sujalc879.github.io/Spotify/${currFolder}/`)
     let response = await a.text() 
     
     let div = document.createElement("div");
@@ -76,7 +76,7 @@ const playMusic = (track, pause=false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`https://sujalc879.github.io/Spotify/songs/`)
     let response = await a.text() 
     
     let div = document.createElement("div");
@@ -92,7 +92,7 @@ async function displayAlbums() {
            let folder = e.href.split("/").slice(-2)[0]
            
         //    Get Meta data of Folder
-        let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+        let a = await fetch(`https://sujalc879.github.io/Spotify/songs/${folder}/info.json`)
         let response = await a.json()  
         
         cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card ">
